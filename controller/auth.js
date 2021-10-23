41cfae93ca02ef9a93a8795e7b903990fa34f7e5
raejun92 <cvcv1324@naver.com>
2021-10-24 08:49:34 +0900

    Add function signup login & Add users in user

--- controller/auth.js
+++ controller/auth.js
@@ -1,7 +1,23 @@
+import * as userRepository from '../data/auth.js';
+
 export async function signup(req, res) {
-	console.log("signup");
+	const { userid, password, name, email } = req.body;
+	const checkid = await userRepository.findByUserid(userid);
+	if (checkid)
+		return res.status(409).json({ message: `${userid} already exists`});
+	const newbie = await userRepository.createUser({
+		userid,
+		password,
+		name,
+		email,
+	});
+	res.status(201).json({ name });
 }
 
 export async function login(req, res) {
-	console.log("login");
+	const { userid, password } = req.body;
+	const user = await userRepository.findByUserid(userid);
+	if (!user)
+		return res.status(404).json({ message: `${userid} not found or Invalid password`});
+	
 }
--- data/auth.js
+++ data/auth.js
@@ -1,3 +1,13 @@
 let users = [
-
+	{
+		id: '1',
+		userid: 'userid',
+		password: 'password',
+		name: 'minsu',
+		email: 'minsu@google.com',
+	},
 ];
+
+export async function findByUserid(userid) {
+	
+}
