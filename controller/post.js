2380280c5f259fda4ec3c913e155f68353bed144
raejun92 <cvcv1324@naver.com>
2021-10-25 16:01:55 +0900

    Add func GET posts

--- controller/post.js
+++ controller/post.js
@@ -1,8 +1,26 @@
 import 'express-async-errors';
 import * as postRepository from '../data/post.js';
 
+function isNumber(str) {
+	for (let i = 0; i < str.length; i++)
+		if (!(str[i] >= '0' && str[i] <= 9))
+			return false;
+	return true;
+}
+
 export async function getPosts(req, res) {
-	
+	const limit = req.query.limit;
+	const offset = req.query.offset;
+
+	if (!(limit && offset) || !(isNumber(limit) && isNumber(offset))) {
+		const posts = await postRepository.getAll();
+		res.status(200).json({ data: posts });
+	} 
+	else {
+		const posts = await postRepository.getByLimitOffset(limit, offset);
+		const cnt = posts.length;
+		res.status(200).json({ count: cnt, data: posts });
+	}
 }
 
 export async function createPost(req, res) {
--- data/post.js
+++ data/post.js
@@ -1,8 +1,76 @@
 let posts = [
 	{
+		id: '10',
+		text: "hello world",
+		author: "minsu",
+		createdAt: new Date().toString(),
+	},
+	{
+		id: '9',
+		text: "hello world",
+		author: "minsu",
+		createdAt: new Date().toString(),
+	},
+	{
+		id: '8',
+		text: "hello world",
+		author: "minsu",
+		createdAt: new Date().toString(),
+	},
+	{
+		id: '7',
+		text: "hello world",
+		author: "minsu",
+		createdAt: new Date().toString(),
+	},
+	{
+		id: '6',
+		text: "hello world",
+		author: "minsu",
+		createdAt: new Date().toString(),
+	},
+	{
+		id: '5',
+		text: "hello world",
+		author: "minsu",
+		createdAt: new Date().toString(),
+	},
+	{
+		id: '4',
+		text: "hello world",
+		author: "minsu",
+		createdAt: new Date().toString(),
+	},
+	{
+		id: '3',
+		text: "hello world",
+		author: "minsu",
+		createdAt: new Date().toString(),
+	},
+	{
+		id: '2',
+		text: "hello world",
+		author: "minsu",
+		createdAt: new Date().toString(),
+	},
+	{
 		id: '1',
 		text: "hello world",
 		author: "minsu",
 		createdAt: new Date().toString(),
 	},
 ];
+
+export async function getAll() {
+	return Promise.all(posts);
+}
+
+export async function getByLimitOffset(limit, offset) {
+	const endPoint = posts.length - offset + 1;
+	
+	if (endPoint <= limit) {
+		return getAll().then(data => data.slice(0, endPoint));
+	} else {
+		return getAll().then(data => data.slice(endPoint - limit, endPoint));
+	}
+}
