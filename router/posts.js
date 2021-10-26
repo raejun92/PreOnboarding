979d14711bd45de7178b51b10bd65834cb314f4c
raejun92 <cvcv1324@naver.com>
2021-10-26 17:08:43 +0900

    .

--- router/posts.js
+++ router/posts.js
@@ -5,9 +5,9 @@ import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/',  postController.getPosts);
-router.post('/',  postController.createPost);
-router.put('/:id',  postController.updatePost);
-router.delete('/:id', postController.deletePost);
+router.get('/', isAuth, postController.getPosts);
+router.post('/', isAuth, postController.createPost);
+router.put('/:id', isAuth, postController.updatePost);
+router.delete('/:id', isAuth, postController.deletePost);
 
 export default router;
